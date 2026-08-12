0254faa6f76ac46aa949263d551f2ef5b5437ff1
mxm <contact@moneyexmachina.com>
2026-08-12 11:12:26 +0100

    remove obsolete imports in models/__init__.py

--- src/mxm/refdata/models/__init__.py
+++ src/mxm/refdata/models/__init__.py
@@ -3,10 +3,6 @@
 from .contracts.futures_contract import FuturesContract
 from .currencies import Currency
 from .months import Month
-from .orm.base import Base
-from .orm.futures_contracts import FuturesContractORM
-from .orm.futures_products import FuturesProductORM
-from .orm.periods import PeriodORM
 from .periods import Period, PeriodType
 from .products.futures_product import FuturesProduct, SettlementMethod
 from .reference_events import ReferenceEvent
@@ -14,15 +10,11 @@ from .units import ProductUnit
 from .weekdays import Weekday
 
 __all__ = [
-    "Base",
     "Currency",
     "FuturesContract",
-    "FuturesContractORM",
     "FuturesProduct",
-    "FuturesProductORM",
     "Month",
     "Period",
-    "PeriodORM",
     "PeriodType",
     "ProductUnit",
     "ReferenceEvent",
